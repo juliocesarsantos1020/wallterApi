537b7efa876cd38a88745a02327656c3f8cd5d96
Júlio Santos <juliocesarsantos1020@gmail.com>
2020-01-21 22:37:45 -0300

    Commit inicial

---
+++ src/main/java/br/com/jcsistemas/wallter/WallterApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcsistemas.wallter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jcsistemas/wallter/WallterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcsistemas.wallter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
