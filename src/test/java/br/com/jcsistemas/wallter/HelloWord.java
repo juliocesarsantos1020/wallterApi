581d6a516df3fd7af7b734b7cc1661b8bddfe4c9
Júlio Santos <juliocesarsantos1020@gmail.com>
2020-01-23 22:23:54 -0300

    Configuração inicial para os teste

--- src/test/java/br/com/jcsistemas/wallter/HelloWord.java
+++ src/test/java/br/com/jcsistemas/wallter/HelloWord.java
@@ -3,16 +3,16 @@ package br.com.jcsistemas.wallter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@ActiveProfiles("test")
-class WallterApplicationTests {
+public class HelloWord {
 
 	@Test
-	void contextLoads() {
+	public void testHelloWord() {
+		assertEquals(1, 1);
 	}
-
 }
